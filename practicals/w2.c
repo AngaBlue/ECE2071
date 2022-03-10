9f6d331623732b2277b92fe9a15ef3ee9454c708
AngaBlue <contact@anga.blue>
2022-03-11 10:02:08 +1100

    Practical

---
+++ practicals/w2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void) {
+    // 1 & 2
+    double answer;
+
+    answer = 9%3 - 1 +22/7.;
+    answer = 8%3 - 1;
+    answer = 2 - 5%2 - 7/2;
+    answer = 2 - 8%3;
+
+    // 5
+    printf("%5.3f\n", 5.78263);
+    printf("%10.2e\n", 66.45832);
+    printf("%+6d\n", 7750);
+
+    // 
+}
