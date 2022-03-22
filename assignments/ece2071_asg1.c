96df489d5023d794cbf7c0c976f407037b883ccb
AngaBlue <contact@anga.blue>
2022-03-22 17:34:55 +1100

    Fix launch tasks

--- assignments/ece2071_asg1.c
+++ assignments/ece2071_asg1.c
@@ -154,7 +154,7 @@ int main(void)
         }
 
         // Output the palindromes
-        printf("%11zi %13zi %14d\n", i, palindrome->length + 1, median_distance - 1);
+        printf("%11zi %13zi %14ld\n", i, palindrome->length + 1, median_distance - 1);
     }
 
     return EXIT_SUCCESS;
