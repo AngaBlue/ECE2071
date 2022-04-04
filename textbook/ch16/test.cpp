5b0d92b8a4f78f075a48c4e0835116a3af0f8376
AngaBlue <contact@anga.blue>
2022-04-04 17:02:28 +1000

    GB

--- textbook/ch16/test.cpp
+++ textbook/ch16/test.cpp
@@ -4,5 +4,9 @@ int main()
 {
     GradeBook book("CS101 C++ Programming");
     book.displayMessage();
+
+    GradeBook *gb= new GradeBook("CS101 C++ Programming");
+    gb->displayMessage();
+    delete gb;
     return 0;
 }
