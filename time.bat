64fd00f94c349daa6a96453aef15830e67ad599e
AngaBlue <contact@anga.blue>
2022-04-05 19:39:07 +1000

    Updated timing file

--- time.bat
+++ time.bat
@@ -1,2 +1,2 @@
-cd /D "%~dp0"\assignments\assn2 && ptime.exe ece2071_asg2.exe maze.txt > out.txt
+cd /D "%~dp0"\assignments\assn2 && ptime.exe ece2071_asg2.exe maze_10000x10000.txt > out.txt
 cd /D "%~dp0"
