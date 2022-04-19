19263436539801bbb172a9ed633b4986e3c95bb6
AngaBlue <contact@anga.blue>
2022-04-19 22:32:06 +1000

    Recursion

---
+++ lectures/w8/recursion.asm
@@ -0,0 +1,16 @@
+fact:
+    subi    $sp,    $sp,    8       # Store 2 registers on the stack
+    sw      $ra,    4($sp)
+    sw      $a0,    0($sp)
+    blt     $a0,    1,      L1      # Branch if n <= 1
+    subi    $a0,    $a0,    1       # n = n - 1
+    jal     fact
+    lw      $a0,    0($sp)          # Restore registers from stack
+    lw      $ra,    4($sp)
+    addi    $sp,    $sp,    8       # Pop stack
+    mult    $v0,    $a0,    $v0     # Multiply n by result
+    jr      $ra
+L1:                                 # n <= 1
+    addi    $v0,    $zero,  1       # Return 1
+    addi    $sp,    $sp,    8       # Pop stack
+    jr      $ra
