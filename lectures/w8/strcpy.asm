291a1e8d97b2ef2044d5b7e313b1f44a1bad4682
AngaBlue <contact@anga.blue>
2022-04-20 23:38:32 +1000

    String copy assembly

--- lectures/w8/strcpy.asm
+++ lectures/w8/strcpy.asm
@@ -2,8 +2,17 @@ strcpy:
     # Create stack frame
     subi    $sp,    $sp,    4
     sw		$s0,    0($sp)
-    addi	$s0,    $zero,  1
+    addi	$s0,    $zero,  0       # i = 0
 While:
-    add		$t0,    $a1,    $s1     # t0 = dest + i
-        
-    
+    add		$t0,    $a1,    $s0     # address = src + i
+    lb		$t1,    0($t0)          # load char from src into t1
+    add		$t0,    $a0,    $s0     # address = dest + i
+    sb		$t1,    0($t0)          # store char to dest from t1
+    beq     $t1,    $zero,  EndWhile# if char == NULL, break
+    addi    $s0,    $s0,    1       # ++i
+    j       While
+EndWhile:
+    # Pop stack frame
+    lw      $s0,    0($sp)
+    addi    $sp,    $sp,    4
+    jr      $ra
