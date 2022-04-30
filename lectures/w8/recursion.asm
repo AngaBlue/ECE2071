2d61a678e47ff659fe66847df1ce04a94f637d38
AngaBlue <contact@anga.blue>
2022-04-30 21:39:55 +1000

    Formatted

--- lectures/w8/recursion.asm
+++ lectures/w8/recursion.asm
@@ -1,28 +1,28 @@
 main:
-    addi    $a0,    $zero,  10       # Calculate 5!
+    addi    $a0,    $zero,  10  # Calculate 5!
     jal     fact
-    # Print
+                                # Print
     move    $a0,    $v0
     li      $v0,    1
-    syscall
-    # Exit
-    li      $v0,    10              # Terminate program run and exit
-    syscall                         
+    syscall 
+                                # Exit
+    li      $v0,    10          # Terminate program run and exit
+    syscall 
 
 fact:
-    subi    $sp,    $sp,    8       # Store 2 registers on the stack
+    subi    $sp,    $sp,    8   # Store 2 registers on the stack
     sw      $ra,    4($sp)
     sw      $a0,    0($sp)
-    blt     $a0,    1,      L1      # Branch if n <= 1
-    subi    $a0,    $a0,    1       # n = n - 1
+    blt     $a0,    1,      L1  # Branch if n <= 1
+    subi    $a0,    $a0,    1   # n = n - 1
     jal     fact
-    lw      $a0,    0($sp)          # Restore registers from stack
+    lw      $a0,    0($sp)      # Restore registers from stack
     lw      $ra,    4($sp)
-    addi    $sp,    $sp,    8       # Pop stack
-    mult    $v0,    $a0             # Multiply n by result
+    addi    $sp,    $sp,    8   # Pop stack
+    mult    $v0,    $a0         # Multiply n by result
     mflo    $v0
     jr      $ra
-L1:                                 # n <= 1
-    addi    $v0,    $zero,  1       # Return 1
-    addi    $sp,    $sp,    8       # Pop stack
+    L1:                         # n <= 1
+    addi    $v0,    $zero,  1   # Return 1
+    addi    $sp,    $sp,    8   # Pop stack
     jr      $ra
--- lectures/w8/strcpy.asm
+++ lectures/w8/strcpy.asm
@@ -1,18 +1,18 @@
-strcpy:                             # a0 = dest, a1 = src
-    # Create stack frame
+    strcpy:                             # a0 = dest, a1 = src
+# Create stack frame
     subi    $sp,    $sp,    4
-    sw		$s0,    0($sp)
-    addi	$s0,    $zero,  0       # i = 0
+    sw      $s0,    0($sp)
+    addi    $s0,    $zero,  0           # i = 0
 While:
-    add		$t0,    $a1,    $s0     # address = src + i
-    lb		$t1,    0($t0)          # load char from src into t1
-    add		$t0,    $a0,    $s0     # address = dest + i
-    sb		$t1,    0($t0)          # store char to dest from t1
-    beq     $t1,    $zero,  EndWhile# if char == NULL, break
-    addi    $s0,    $s0,    1       # ++i
+    add     $t0,    $a1,    $s0         # address = src + i
+    lb      $t1,    0($t0)              # load char from src into t1
+    add     $t0,    $a0,    $s0         # address = dest + i
+    sb      $t1,    0($t0)              # store char to dest from t1
+    beq     $t1,    $zero,  EndWhile    # if char == NULL, break
+    addi    $s0,    $s0,    1           # ++i
     j       While
 EndWhile:
-    # Pop stack frame
+                                        # Pop stack frame
     lw      $s0,    0($sp)
     addi    $sp,    $sp,    4
     jr      $ra
